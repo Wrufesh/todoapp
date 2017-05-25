b162777055800dcd64d67e97bb08ee1280c49eb9
wrufesh singh <wrufesh@gmail.com>
2017-05-25 14:01:04 +0545

    successfully bundled todo app

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,35 @@
 let path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
   entry: {
-    index: 'index.js'
+    index: './index.js'
   },
   output: {
     filename: 'bundle.js',
-    path: '/home/proj/public/assets'
-  }
+    path: path.join(__dirname, 'static')
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']}, // inline css include in bundle
+      {
+        test: /\.scss$/, use: ExtractTextPlugin.extract({
+        fallback: "style-loader",
+        use: ['css-loader', 'sass-loader']
+      })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: (getPath) => {
+        return getPath('css/style.css');
+      }
+    })
+  ],
+  devtool: 'source-map'
+
 };
 
 module.exports = config;
